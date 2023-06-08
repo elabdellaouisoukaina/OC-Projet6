9fecc8030083fd7c50de3d8a1cb5748d132e4b1c
Soukaïna El Abdellaoui <elabdellaoui.soukaina@gmail.com>
2023-06-08 23:16:40 +0200

    Etape 2 - terminée + Etape 3 - Page d'accueil : affichage cartes photographes terminé + lien vers page photographe. Accessibilité à faire

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,40 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
-
+    const { name, portrait, city, country, tagline, price, id} = data;
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+
+        const linkToDetails = document.createElement('a');
+        linkToDetails.title = "Lien vers la page de " + name;
+        linkToDetails.href = "http://127.0.0.1:5500/photographer.html?id=" + id;
+
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+
+        linkToDetails.appendChild(img);     
+
+        const nameElement = document.createElement( 'h2' );
+        nameElement.textContent = name;
+
+        const locationElement = document.createElement('p');
+        locationElement.textContent = city + ', ' + country;
+        locationElement.classList.add('photographer_section--location');
+
+        const taglineElement = document.createElement('p');
+        taglineElement.textContent = tagline;
+        taglineElement.classList.add('photographer_section--tagline');
+
+        const priceElement = document.createElement('p');
+        priceElement.textContent = price + '€/jour';
+        priceElement.classList.add('photographer_section--price');
+
+        article.appendChild(linkToDetails);
+        article.appendChild(nameElement);
+        article.appendChild(locationElement);
+        article.appendChild(taglineElement);
+        article.appendChild(priceElement);
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
