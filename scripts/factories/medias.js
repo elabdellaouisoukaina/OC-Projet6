19368822c0a0f5410d881a0bab4669689836c433
Soukaïna El Abdellaoui <elabdellaoui.soukaina@gmail.com>
2023-07-07 03:42:34 +0200

    Etape 7 - debug de l'affcihage deux fois (appel de init était dans mediasfactory et photographer) + déplacement de fonction d'init dans medias factory

--- scripts/factories/medias.js
+++ scripts/factories/medias.js
@@ -1,8 +1,162 @@
 // import { setCurrentMedias } from "../state/state.js";
-import { initEventListenerLikes, initEventListenerLightbox} from "../pages/photographer.js";
 
 export function mediasFactory(data) {
     const { name, price} = data;
+
+    // Ferme lightbox
+    function initCloseButton() {
+        document.querySelector('.close').addEventListener("click", () => {
+            document.getElementById("lightbox").style.display = "none";
+            document.querySelector('.lightbox-media').innerHTML = "";
+        })
+    }
+
+    // Affiche lightbox
+    function displayLightbox() {
+        document.querySelector("#lightbox").style.display = "block";
+        document.querySelector('.lightbox-media').innerHTML = "";
+    }
+
+    function initLightboxButtons(currentIndex, medias) {
+        // Bouton précédent
+        document.querySelector('.prev').addEventListener('click', () => {
+            if (currentIndex != 0) {
+                currentIndex = currentIndex - 1;
+            } else {
+                currentIndex = medias.length - 2; // Affiche la dernière image après la première
+            }
+    
+            let isImg = medias[currentIndex].classList.contains('photographer_gallery--element__img');
+            let isVideo = medias[currentIndex].classList.contains('photographer_gallery--element__video');
+            const lightboxMedia = document.querySelector('.lightbox-media');
+            lightboxMedia.innerHTML = "";
+    
+            if (isImg) { 
+                let img = document.createElement( 'img' );
+                let imgUrl = medias[currentIndex].src;
+                img.setAttribute("src", imgUrl);
+                img.alt = medias[currentIndex].title;
+                img.classList.add('photographer_gallery--element__img', 'photographer_gallery--element__media');
+                lightboxMedia.appendChild(img);
+    
+                let title = document.querySelector('.lightbox-media__title')
+                title.innerHTML = medias[currentIndex].alt; 
+            } 
+            
+            else if (isVideo) {           
+                let video = document.createElement('video');
+                video.classList.add("photographer_gallery--element__video", "photographer_gallery--element__media");
+                let source = document.createElement('source');
+                let videoUrl = medias[currentIndex].firstChild.src;
+                source.setAttribute('src', videoUrl);
+                source.setAttribute('type', "video/mp4");
+                video.setAttribute('controls', true);
+                video.appendChild(source);
+                lightboxMedia.appendChild(video);
+    
+                let title = document.querySelector('.lightbox-media__title')
+                title.innerHTML = medias[currentIndex].title; 
+            }
+        })
+    
+        // Bouton suivant
+        document.querySelector('.next').addEventListener("click", () => {
+            if (currentIndex !=  medias.length - 2) {
+                currentIndex = currentIndex + 1;
+            } else {
+                currentIndex = 0; // Affiche la première image après la dernière
+            }
+    
+            let isImg = medias[currentIndex].classList.contains('photographer_gallery--element__img');
+            let isVideo = medias[currentIndex].classList.contains('photographer_gallery--element__video');
+            const lightboxMedia = document.querySelector('.lightbox-media');
+            lightboxMedia.innerHTML = "";
+    
+            if (isImg) { 
+                let img = document.createElement( 'img' );
+                let imgUrl = medias[currentIndex].src;
+                img.setAttribute("src", imgUrl);
+                img.alt = medias[currentIndex].title;
+                img.classList.add('photographer_gallery--element__img', 'photographer_gallery--element__media');
+                lightboxMedia.appendChild(img);
+    
+                let title = document.querySelector('.lightbox-media__title')
+                title.innerHTML = medias[currentIndex].alt; 
+            } 
+            
+            else if (isVideo) {           
+                let video = document.createElement('video');
+                video.classList.add("photographer_gallery--element__video", "photographer_gallery--element__media");
+                video.title = medias[currentIndex].title;
+                let source = document.createElement('source');
+                let videoUrl = medias[currentIndex].firstChild.src;
+                source.setAttribute('src', videoUrl);
+                source.setAttribute('type', "video/mp4");
+                video.setAttribute('controls', true);
+                video.appendChild(source);
+                lightboxMedia.appendChild(video);
+    
+                let title = document.querySelector('.lightbox-media__title')
+                title.innerHTML = medias[currentIndex].title;
+            }
+        });
+    }
+
+    async function initEventListenerLightbox(){
+        let medias = document.getElementsByClassName("photographer_gallery--element__media");
+    
+        initCloseButton();
+    
+        Array.prototype.forEach.call(medias, function(element, index) {
+            element.addEventListener("click", () => { 
+                let currentIndex = index;
+    
+                // Affiche la lightbox
+                displayLightbox();
+    
+                // Mets à jour HTML pour image selectionnée
+                const lightboxMedia = document.querySelector('.lightbox-media');
+    
+                if (element.classList.contains('photographer_gallery--element__img')){
+                    const img = document.createElement('img');
+                    img.classList.add('lighbox-img');
+                    img.setAttribute('src', element.src);
+                    img.alt = element.alt
+    
+                    let title = document.querySelector('.lightbox-media__title')
+                    title.innerHTML = element.alt; 
+    
+                    lightboxMedia.appendChild(img);
+                }
+                else if (element.classList.contains('photographer_gallery--element__video')){                
+                    let video = document.createElement('video');
+                    let source = document.createElement('source');
+                    let videoUrl = element.firstChild.src;
+                    video.title = element.title;
+                    source.setAttribute('src', videoUrl);
+                    source.setAttribute('type', 'video/mp4');
+                    video.setAttribute('controls', true);
+                    video.classList.add('lighbox-video');
+                    video.appendChild(source);
+                    lightboxMedia.appendChild(video);
+    
+                    let title = document.querySelector('.lightbox-media__title')
+                    title.innerHTML = element.title; 
+                }
+    
+                initLightboxButtons(currentIndex, medias);
+            })
+        })
+    }
+
+    async function initEventListenerLikes(){
+        Array.prototype.forEach.call(document.getElementsByClassName("likable"), function(element) {
+            element.addEventListener("click",() => { 
+                element.previousSibling.innerHTML =  parseInt(element.previousSibling.innerHTML) + 1;
+                document.querySelector('.photograph-price div p').innerHTML = parseInt(document.querySelector('.photograph-price div p').innerHTML) + 1;
+            }, {once : true});
+        })
+    }  
 
     function createGallery(medias) {
         const gallery = document.querySelector(".photograph-gallery");
@@ -115,7 +269,8 @@ export function mediasFactory(data) {
             }    
         )
     }
+    
 
-    return { createGallery, getGalleryFilterByPopularity, getGalleryFilterByDate, getGalleryFilterByTitle}
+    return { initCloseButton, displayLightbox, initLightboxButtons, initEventListenerLightbox, initEventListenerLikes, createGallery, getGalleryFilterByPopularity, getGalleryFilterByDate, getGalleryFilterByTitle}
 }
 
